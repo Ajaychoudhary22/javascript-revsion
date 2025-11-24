15205891a903cc748a19cb016d2045cef52daa9f
git remote add origin https://github.com/Ajaychoudhary22/javascript-revsion.git git branch -M main git push -u origin main <ajayc3498@gmail.com>
2025-11-24 18:45:08 +0530

    project onDom

---
+++ Project1.js
@@ -0,0 +1,66 @@
+        let btn = document.getElementById('btn');
+        let box = document.getElementById('box');
+       let h1 = document.getElementById('h1');
+
+  let arr = [
+  {
+    name: "CSK",
+    trophies: 5,
+    jerseyColor: "Yellow"
+  },
+  {
+    name: "MI",
+    trophies: 5,
+    jerseyColor: "Blue"
+  },
+  {
+    name: "KKR",
+    trophies: 3,
+    jerseyColor: "Purple & Gold"
+  },
+  {
+    name: "SRH",
+    trophies: 1,
+    jerseyColor: "Orange"
+  },
+  {
+    name: "RR",
+    trophies: 1,
+    jerseyColor: "Pink & Blue"
+  },
+  {
+    name: "RCB",
+    trophies: 0,
+    jerseyColor: "Red & Black"
+  },
+  {
+    name: "DC",
+    trophies: 0,
+    jerseyColor: "Blue & Red"
+  },
+  {
+    name: "PBKS",
+    trophies: 0,
+    jerseyColor: "Red"
+  },
+  {
+    name: "GT",
+    trophies: 1,
+    jerseyColor: "Dark Blue & Gold"
+  },
+  {
+    name: "LSG",
+    trophies: 0,
+    jerseyColor: "Turquoise & Orange"
+  }
+];
+
+        
+
+       btn.addEventListener('click', function () {
+    h1.innerText = "THE WINNER IS";
+
+    let randomTeam = Math.floor(Math.random() * arr.length);
+
+    box.innerText = arr[randomTeam].name.toUpperCase();
+});
