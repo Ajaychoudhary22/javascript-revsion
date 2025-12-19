59d92058ddceea56b81b1c2813a502c679a1c5f7
git remote add origin https://github.com/Ajaychoudhary22/javascript-revsion.git git branch -M main git push -u origin main <ajayc3498@gmail.com>
2025-12-19 15:59:14 +0530

    local storage

---
+++ localStroge.js
@@ -0,0 +1,56 @@
+// var btn = document.querySelector("#btn")
+
+
+// btn.addEventListener("click", function() {
+//     location.href= "https://www.jeetfoundations.in/";
+// });
+
+
+// local stroage ki kuch functions hote h jese ki setItem, getItem, removeItem, clear
+
+
+// // setItem
+// localStorage.setItem("name", "jeet");
+// localStorage.setItem("age", "22");
+
+
+// getItem
+
+// var name = localStorage.getItem("name");
+// console.log(name);
+
+
+// removeItem
+// localStorage.removeItem("age");
+
+// clear
+// localStorage.clear();
+
+
+// we cannot save the object value in the local stroage we can convert it into string through json method its example to understand the problem 
+
+
+var user = [
+    {
+    name: "jeet",
+    age: 22,
+    city: "rajkot"
+    
+},
+{
+    name: "aeet",
+    age: 22,
+    city: "rajkot"
+},
+{
+    name: "veet",
+    age: 22,
+    city: "rajkot"
+}
+]
+
+// var newUser= JSON.stringify(user);
+
+var newUser= JSON.parse(user);  
+localStorage.setItem("user", newUser);
+console.log(user);
